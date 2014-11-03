7acca03ce3f77e3e652fac0973e1ed0bc7063c28
hoaxiang <aeternchan@gmail.com>
2014-11-03 10:32:21 +0800

    BlogDetails

--- iosapp/OSCBlogDetails.h
+++ iosapp/OSCBlogDetails.h
@@ -10,4 +10,16 @@
 
 @interface OSCBlogDetails : OSCBaseObject
 
+@property (nonatomic, assign) int64_t blogID;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSURL *url;
+@property (nonatomic, copy) NSString *where;
+@property (nonatomic, assign) int commentCount;
+@property (nonatomic, copy) NSString *body;
+@property (nonatomic, copy) NSString *author;
+@property (nonatomic, assign) int64_t authorID;
+@property (nonatomic, assign) int documentType;
+@property (nonatomic, copy) NSString *pubDate;
+@property (nonatomic, assign) int favoriteCount;
+
 @end
--- iosapp/OSCBlogDetails.m
+++ iosapp/OSCBlogDetails.m
@@ -8,6 +8,39 @@
 
 #import "OSCBlogDetails.h"
 
+static NSString *kID = @"id";
+static NSString *kTitle = @"title";
+static NSString *kURL = @"url";
+static NSString *kBody = @"body";
+static NSString *kCommentCount = @"commentCount";
+static NSString *kAuthor = @"author";
+static NSString *kAuthorID = @"authorid";
+static NSString *kPubDate = @"pubDate";
+static NSString *kFavorite = @"favorite";
+static NSString *kWhere = @"where";
+static NSString *kDocumentType = @"documentType";
+
 @implementation OSCBlogDetails
+
+- (instancetype)initWithXML:(ONOXMLElement *)xml
+{
+    self = [super init];
+    
+    if (self) {
+        self.blogID = [[[xml firstChildWithTag:kID] numberValue] longLongValue];
+        self.title = [[xml firstChildWithTag:kTitle] stringValue];
+        self.url = [NSURL URLWithString:[[xml firstChildWithTag:kURL] stringValue]];
+        self.body = [[xml firstChildWithTag:kBody] stringValue];
+        self.commentCount = [[[xml firstChildWithTag:kCommentCount] numberValue] intValue];
+        self.author = [[xml firstChildWithTag:kAuthor] stringValue];
+        self.authorID = [[[xml firstChildWithTag:kAuthorID] numberValue] longLongValue];
+        self.pubDate = [[xml firstChildWithTag:kPubDate] stringValue];
+        self.favoriteCount = [[[xml firstChildWithTag:kFavorite] numberValue] intValue];
+        self.where = [[xml firstChildWithTag:kWhere] stringValue];
+        self.documentType = [[[xml firstChildWithTag:kDocumentType] numberValue] intValue];
+    }
+    
+    return self;
+}
 
 @end
