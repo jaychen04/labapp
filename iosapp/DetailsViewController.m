f5acaccd9958a6d212e549355c4055ee781c6e78
haoxiang <aeternchan@gmail.com>
2015-03-03 15:40:28 +0800

    详情页HTML微调

--- iosapp/DetailsViewController.m
+++ iosapp/DetailsViewController.m
@@ -32,7 +32,7 @@
 #import <MBProgressHUD.h>
 
 
-#define HTML_STYLE @"<style>#oschina_title {color: #000000; margin-bottom: 6px; font-weight:bold;}#oschina_title img{vertical-align:middle;margin-right:6px;}#oschina_title a{color:#0D6DA8;}#oschina_outline {color: #707070; font-size: 12px;}#oschina_outline a{color:#0D6DA8;}#oschina_software{color:#808080;font-size:12px}#oschina_body img {max-width: 300px;}#oschina_body {font-size:16px;max-width:300px;line-height:24px;} #oschina_body table{max-width:300px;}#oschina_body pre { font-size:9pt;font-family:Courier New,Arial;border:1px solid #ddd;border-left:5px solid #6CE26C;background:#f6f6f6;padding:5px;}</style>"
+#define HTML_STYLE @"<style>#oschina_title {color: #000000; margin-bottom: 6px; font-weight:bold;}#oschina_title img{vertical-align:middle;margin-right:6px;}#oschina_title a{color:#0D6DA8;}#oschina_outline {color: #707070; font-size: 12px;}#oschina_outline a{color:#0D6DA8; text-decoration:none;}#oschina_software{color:#808080;font-size:12px}#oschina_body img {max-width: 300px;}#oschina_body {font-size:16px;max-width:300px;line-height:24px;} #oschina_body table{max-width:300px;}#oschina_body pre { font-size:9pt;font-family:Courier New,Arial;border:1px solid #ddd;border-left:5px solid #6CE26C;background:#f6f6f6;padding:5px;}</style>"
 
 #define HTML_BOTTOM @"<div style='margin-bottom:60px'/>"
 
@@ -387,7 +387,7 @@
 {
     newsDetails.title = [Utils escapeHTML:newsDetails.title];
     
-    NSString *authorStr = [NSString stringWithFormat:@"<a href='http://my.oschina.net/u/%lld'>%@</a> 发布于 %@", newsDetails.authorID, newsDetails.author, newsDetails.pubDate];
+    NSString *authorStr = [NSString stringWithFormat:@"<a href='http://my.oschina.net/u/%lld'>%@</a> 发布于 %@", newsDetails.authorID, newsDetails.author, [Utils intervalSinceNow:newsDetails.pubDate]];
     
     NSString *software = @"";
     if ([newsDetails.softwareName isEqualToString:@""] == NO) {
