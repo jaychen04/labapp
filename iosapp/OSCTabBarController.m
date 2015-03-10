ec6dd5608819a63249a6fd2b3228a0368d24e442
haoxiang <aeternchan@gmail.com>
2015-03-10 13:21:52 +0800

    修改中间弹出的“摇一摇”按钮的图标

--- iosapp/OSCTabBarController.m
+++ iosapp/OSCTabBarController.m
@@ -102,7 +102,7 @@
     _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     
     NSArray *buttonTitles = @[@"文字", @"相册", @"拍照", @"摇一摇", @"扫一扫", @"找人"];
-    NSArray *buttonImages = @[@"tweetEditing", @"picture", @"shooting", @"sound", @"scan", @"search"];
+    NSArray *buttonImages = @[@"tweetEditing", @"picture", @"shooting", @"shake", @"scan", @"search"];
     int buttonColors[] = {0xe69961, 0x0dac6b, 0x24a0c4, 0xe96360, 0x61b644, 0xf1c50e};
     
     for (int i = 0; i < 6; i++) {
