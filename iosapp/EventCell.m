d6c224d908c723a632a1195dcb7cc4495a0fbcb9
liping123 <15949569236@163.com>
2015-03-10 16:55:32 +0800

    微调“消息中心”页面的字体大小

--- iosapp/EventCell.m
+++ iosapp/EventCell.m
@@ -59,7 +59,7 @@
     [self.contentView addSubview:_actionLabel];
     
     _timeLabel = [UILabel new];
-    _timeLabel.font = [UIFont systemFontOfSize:14];
+    _timeLabel.font = [UIFont systemFontOfSize:12];
     _timeLabel.textColor = [UIColor grayColor];
     [self.contentView addSubview:_timeLabel];
     
@@ -82,7 +82,7 @@
     [self.contentView addSubview:_referenceText];
     
     _appclientLabel = [UILabel new];
-    _appclientLabel.font = [UIFont systemFontOfSize:14];
+    _appclientLabel.font = [UIFont systemFontOfSize:12];
     _appclientLabel.textColor = [UIColor grayColor];
     [self.contentView addSubview:_appclientLabel];
     
