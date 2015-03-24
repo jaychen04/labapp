22d7dae7cbdd0289aa5bc827b4375692a59ec0b4
haoxiang <aeternchan@gmail.com>
2015-03-24 14:33:34 +0800

    添加团队功能占位

--- iosapp/MyInfoViewController.m
+++ iosapp/MyInfoViewController.m
@@ -233,7 +233,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 2;
+    return 3;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -246,8 +246,8 @@
     [cell setSelectedBackgroundView:selectedBackground];
     
     cell.backgroundColor = [UIColor colorWithHex:0xF9F9F9];
-    cell.textLabel.text = @[@"消息", @"博客"][indexPath.row];
-    cell.imageView.image = [UIImage imageNamed:@[@"me-message", @"me-blog"][indexPath.row]];
+    cell.textLabel.text = @[@"消息", @"博客", @"团队"][indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@[@"me-message", @"me-blog", @"me-team"][indexPath.row]];
     
     if (indexPath.row == 0) {
         if (_badgeValue == 0) {
@@ -297,6 +297,15 @@
             blogsVC.navigationItem.title = @"我的博客";
             blogsVC.hidesBottomBarWhenPushed = YES;
             [self.navigationController pushViewController:blogsVC animated:YES];
+            break;
+        }
+        case 2: {
+            MBProgressHUD *HUD = [Utils createHUD];
+            HUD.mode = MBProgressHUDModeText;
+            HUD.labelText = @"即将推出团队功能，敬请期待";
+            [HUD hide:YES afterDelay:1];
+            [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+            
             break;
         }
         default: break;
