1c66e67a57b5718de6502f00aaf4b47031359113
liping123 <15949569236@163.com>
2015-04-30 14:15:14 +0800

    取消点赞成功后的提示框

--- iosapp/TweetDetailsViewController.m
+++ iosapp/TweetDetailsViewController.m
@@ -250,7 +250,6 @@
 
 - (void)toPraise:(OSCTweet *)tweet
 {
-    MBProgressHUD *HUD = [Utils createHUD];
     NSString *postUrl;
     if (tweet.isLike) {
         postUrl = [NSString stringWithFormat:@"%@%@", OSCAPI_PREFIX, OSCAPI_TWEET_UNLIKE];
@@ -271,8 +270,6 @@
               ONOXMLElement *resultXML = [responseObject.rootElement firstChildWithTag:@"result"];
               int errorCode = [[[resultXML firstChildWithTag: @"errorCode"] numberValue] intValue];
               NSString *errorMessage = [[resultXML firstChildWithTag:@"errorMessage"] stringValue];
-              
-              HUD.mode = MBProgressHUDModeCustomView;
               
               if (errorCode == 1) {
                   if (tweet.isLike) {
@@ -296,12 +293,6 @@
                   tweet.isLike = !tweet.isLike;
                   tweet.likersDetailString = nil;
                   
-                  HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"HUD-done"]];
-                  if (tweet.isLike) {
-                      HUD.labelText = @"点赞成功";
-                  } else {
-                      HUD.labelText = @"取消点赞成功";
-                  }
 #if 0
                   dispatch_async(dispatch_get_main_queue(), ^{
                       [self.tableView reloadData];
@@ -312,13 +303,18 @@
                   [self.tableView endUpdates];
 #endif
               } else {
+                  MBProgressHUD *HUD = [Utils createHUD];
+                  HUD.mode = MBProgressHUDModeCustomView;
+                  
                   HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"HUD-error"]];
                   HUD.labelText = [NSString stringWithFormat:@"错误：%@", errorMessage];
+                  
+                  [HUD hide:YES afterDelay:1];
               }
-              
-              [HUD hide:YES afterDelay:1];
           } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              MBProgressHUD *HUD = [Utils createHUD];
               HUD.mode = MBProgressHUDModeCustomView;
+              
               HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"HUD-error"]];
               HUD.detailsLabelText = error.userInfo[NSLocalizedDescriptionKey];
               
