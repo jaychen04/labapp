740b77cedfa195f3a8da3e2b54ef63eac70ba17f
haoxiang <aeternchan@gmail.com>
2015-05-22 10:41:18 +0800

    完善评论列表基类，添加UIRefreshControl

--- iosapp/TeamRepliesBVC.m
+++ iosapp/TeamRepliesBVC.m
@@ -25,6 +25,7 @@ static NSString * const kTeamReplyCellID = @"TeamReplyCell";
 
 @property (nonatomic, strong) NSMutableArray *replies;
 @property (nonatomic, strong) LastCell *lastCell;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 @property (nonatomic, strong) UILabel *label;
 
 @property (nonatomic, assign) int repliesCount;
@@ -77,6 +78,11 @@ static NSString * const kTeamReplyCellID = @"TeamReplyCell";
     _tableView.translatesAutoresizingMaskIntoConstraints = NO;
     [_tableView registerClass:[TeamReplyCell class] forCellReuseIdentifier:kTeamReplyCellID];
     _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    _refreshControl = [[UIRefreshControl alloc] init];
+    [_refreshControl addTarget:self action:@selector(refresh) forControlEvents:UIControlEventValueChanged];
+    [_tableView addSubview:_refreshControl];
+    
     [self.view addSubview:_tableView];
     
     _lastCell = [LastCell new];
@@ -191,6 +197,11 @@ static NSString * const kTeamReplyCellID = @"TeamReplyCell";
 
 #pragma mark - 获取评论列表
 
+- (void)refresh
+{
+    [self fetchRepliesOnPage:0];
+}
+
 - (void)fetchRepliesOnPage:(NSUInteger)page
 {
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
@@ -231,11 +242,32 @@ static NSString * const kTeamReplyCellID = @"TeamReplyCell";
                  }
              }
              
+             if (_refreshControl.refreshing) {[_refreshControl endRefreshing];}
+             
              dispatch_async(dispatch_get_main_queue(), ^{
+                 if (page == 0 && repliesXML.count == 0) {
+                     _lastCell.status = LastCellStatusEmpty;
+                 } else if (repliesXML.count == 0 || (page == 0 && repliesXML.count < 20)) {
+                     _lastCell.status = LastCellStatusFinished;
+                 } else {
+                     _lastCell.status = LastCellStatusMore;
+                 }
+                 
                  [_tableView reloadData];
              });
          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             MBProgressHUD *HUD = [Utils createHUD];
+             HUD.mode = MBProgressHUDModeCustomView;
+             HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"HUD-error"]];
+             HUD.detailsLabelText = [NSString stringWithFormat:@"%@", error.userInfo[NSLocalizedDescriptionKey]];
              
+             [HUD hide:YES afterDelay:1];
+             
+             _lastCell.status = LastCellStatusError;
+             if (_refreshControl.refreshing) {
+                 [_refreshControl endRefreshing];
+             }
+             [self.tableView reloadData];
          }];
 }
 
