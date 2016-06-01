418e34e4f2967d86e964ee27bfc3bc340469c4d8
巴拉迪维 <baratsemet@gmail.com>
2016-06-01 10:43:34 +0800

    博客详情

--- iosapp/Controllers/NewsAndBlogDetailVC/recommandBlogTableViewCell.h
+++ iosapp/Controllers/NewsAndBlogDetailVC/recommandBlogTableViewCell.h
@@ -1,14 +1,14 @@
 //
-//  recommandBlogTableViewCell.h
+//  RecommandBlogTableViewCell.h
 //  iosapp
 //
-//  Created by Holden on 16/5/30.
+//  Created by 巴拉提 on 16/5/30.
 //  Copyright © 2016年 oschina. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface recommandBlogTableViewCell : UITableViewCell
+@interface RecommandBlogTableViewCell : UITableViewCell
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *commentCountLabel;
 @property (weak, nonatomic) IBOutlet UILabel *viewCountLabel;
