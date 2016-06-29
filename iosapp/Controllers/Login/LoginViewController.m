535e4fd81143d8d5e381ffee1ec477ed6a9625f9
liping <liping@oschina.cn>
2016-06-29 14:24:05 +0800

    微调顶踩接口

--- iosapp/Controllers/Login/LoginViewController.m
+++ iosapp/Controllers/Login/LoginViewController.m
@@ -157,7 +157,7 @@ static NSString * const kShowAccountOperation = @"ShowAccountOperation";
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager OSCManager];
 
 
-    [manager POST:[NSString stringWithFormat:@"%@%@", OSCAPI_HTTPS_PREFIX, OSCAPI_LOGIN_VALIDATE]
+    [manager POST:[NSString stringWithFormat:@"%@%@", OSCAPI_PREFIX_LOGIN, OSCAPI_LOGIN_VALIDATE]
        parameters:@{@"username" : _accountField.text, @"pwd" : _passwordField.text, @"keep_login" : @(1)}
           success:^(AFHTTPRequestOperation *operation, ONOXMLDocument *responseObject) {
               ONOXMLElement *result = [responseObject.rootElement firstChildWithTag:@"result"];
