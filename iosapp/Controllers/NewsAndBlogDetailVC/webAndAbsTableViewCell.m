1771b9616d7264809817642e8664ba2c8f059e81
OSC_Wahson <lvjunqi@oschina.cn>
2016-07-08 11:10:33 +0800

    实现label行高行距 && 调整资讯详情 博客详情的布局

--- iosapp/Controllers/NewsAndBlogDetailVC/webAndAbsTableViewCell.m
+++ iosapp/Controllers/NewsAndBlogDetailVC/webAndAbsTableViewCell.m
@@ -19,18 +19,14 @@
 }
 
 - (void)setAbstractText:(NSString* )abstract{
-    NSMutableAttributedString* attributedStr = [[NSMutableAttributedString alloc]initWithData:[abstract dataUsingEncoding:NSUnicodeStringEncoding] options:@{NSFontAttributeName: @"STSongti-SC"}documentAttributes:nil error:nil];
     
-    NSMutableParagraphStyle * paragraphStyle1 = [[NSMutableParagraphStyle alloc] init];
-    [paragraphStyle1 setLineSpacing:Line_Spacing];
-    [paragraphStyle1 setLineHeightMultiple:Line_Height];
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:abstract];
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    [paragraphStyle setLineSpacing:Line_Spacing];
+    [paragraphStyle setMinimumLineHeight:Line_Height];
+    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [abstract length])];
+    _abstractLabel.attributedText = attributedString;
 
-    
-    [attributedStr addAttribute:NSParagraphStyleAttributeName value:paragraphStyle1 range:NSMakeRange(0, attributedStr.length)];
-    
-    _abstractLabel.attributedText = attributedStr;
-    _abstractLabel.textAlignment = NSTextAlignmentLeft;
-    _abstractLabel.font = [UIFont fontWithName:@"STSongti-SC" size:14];
 }
 
 @end
