7fc58ca255703b6d0568eaa34bff3feb3103988b
OSC_Wahson <lvjunqi@oschina.cn>
2016-07-26 17:07:49 +0800

    commit

--- iosapp/Controllers/Tweet/TweetTableViewController.m
+++ iosapp/Controllers/Tweet/TweetTableViewController.m
@@ -574,7 +574,7 @@ static NSString* const reuseIdentifier_Multiple = @"NewMultipleTweetCell";
 #pragma mark - 点赞功能
 - (void)togglePraise:(UIButton *)button
 {
-    OSCTweetItem *tweet = self.objects[button.tag];
+    OSCTweetItem *tweet = self.dataModels[button.tag];
     [self toPraise:tweet];
 }
 #pragma mark --点赞（新接口)
@@ -591,12 +591,17 @@ static NSString* const reuseIdentifier_Multiple = @"NewMultipleTweetCell";
               
               if([responseObject[@"code"]integerValue] == 1) {
                   tweet.liked = !tweet.liked;
+                  NSDictionary* resultDic = responseObject[@"result"];
+                  tweet.likeCount = [resultDic[@"likeCount"] integerValue];
               }else {
                   MBProgressHUD *HUD = [Utils createHUD];
                   HUD.mode = MBProgressHUDModeCustomView;
                   HUD.label.text = [NSString stringWithFormat:@"%@", responseObject[@"message"]?:@"未知错误"];
                   [HUD hideAnimated:YES afterDelay:1];
               }
+              dispatch_async(dispatch_get_main_queue(), ^{
+                  [self.tableView reloadData];
+              });
           }
           failure:^(AFHTTPRequestOperation *operation, NSError *error) {
               MBProgressHUD *HUD = [Utils createHUD];
