52cb01888b9ec70f9e396c0160a1923488c96a3f
liping <liping@oschina.cn>
2016-07-28 14:50:27 +0800

    替换资讯滚动图

--- iosapp/Views/BannerScrollView.h
+++ iosapp/Views/BannerScrollView.h
@@ -7,7 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "OSCBanner.h"
+
+@protocol BannerScrollViewDelegate <NSObject>
+
+- (void)clickedScrollViewBanners:(NSInteger)bannerTag;
+
+@end
+
+#pragma mark - 滚动图
 
 @interface BannerScrollView : UIView
+
+@property (nonatomic, strong) NSMutableArray *banners;
+@property (nonatomic, strong) id <BannerScrollViewDelegate> delegate;
+
+@end
+
+#pragma mark - 背景图片加文字
+@interface BannerImageView : UIView
+
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UILabel *titleLable;
+
+- (void)setContentForBanners:(OSCBanner *)banner;
 
 @end
