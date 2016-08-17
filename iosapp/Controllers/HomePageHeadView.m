40e369886dd02eb695576a960763334af6df48af
liping <liping@oschina.cn>
2016-08-17 16:43:36 +0800

    完成我的首页新布局

---
+++ iosapp/Controllers/HomePageHeadView.m
@@ -0,0 +1,120 @@
+//
+//  HomePageHeadView.m
+//  iosapp
+//
+//  Created by 李萍 on 16/8/16.
+//  Copyright © 2016年 oschina. All rights reserved.
+//
+
+#import "HomePageHeadView.h"
+#import "Utils.h"
+#import "OSCUser.h"
+#import "AppDelegate.h"
+#import "QuartzCanvasView.h"
+
+#import <Masonry.h>
+
+#define screen_width [UIScreen mainScreen].bounds.size.width
+#define screen_half_width [UIScreen mainScreen].bounds.size.width * 0.5
+
+#define userPortrait_width 80
+#define genderImageView_width 20
+#define bottomButton_height 60
+#define bottom_subButton_height 30
+
+#define view_userPortrait 30
+
+@implementation HomePageHeadView
+{
+    __weak QuartzCanvasView* _drawView;
+}
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setLayout];
+    }
+    return self;
+}
+
+- (void)setLayout
+{
+    CGFloat viewHeight = CGRectGetHeight(self.frame);
+    
+    QuartzCanvasView* drawView = [[QuartzCanvasView alloc]initWithFrame:(CGRect){{0,0},self.bounds.size}];
+    _drawView = drawView;
+    _drawView.openRandomness = YES;
+    _drawView.strokeColor = [UIColor colorWithHex:0x6FDB94];
+    _drawView.gradientColor = (GradientColor){
+        [UIColor colorWithHex:0x24CF5F].CGColor,
+        [UIColor colorWithHex:0x20B955].CGColor,
+    };
+    _drawView.offestCenter = (OffestCenter){0, (view_userPortrait + userPortrait_width * 0.5) - viewHeight * 0.5};
+    [self addSubview:_drawView];
+    
+    _userPortrait = [UIImageView new];
+    _userPortrait.contentMode = UIViewContentModeScaleAspectFit;
+    [_userPortrait setCornerRadius:userPortrait_width * 0.5];
+    _userPortrait.userInteractionEnabled = YES;
+    [self addSubview:_userPortrait];
+    
+    _genderImageView = [UIImageView new];
+    _genderImageView.contentMode = UIViewContentModeScaleAspectFit;
+    _genderImageView.hidden = YES;
+    [self addSubview:_genderImageView];
+    
+    _nameLabel = [UILabel new];
+    _nameLabel.font = [UIFont systemFontOfSize:20];
+    _nameLabel.textAlignment = NSTextAlignmentCenter;
+    _nameLabel.numberOfLines = 1;
+    _nameLabel.textColor = [UIColor colorWithHex:0xFFFFFF];
+    [self addSubview:_nameLabel];
+    
+    _descLable = [UILabel new];
+    _descLable.font = [UIFont systemFontOfSize:13];
+    _descLable.textAlignment = NSTextAlignmentCenter;
+    _descLable.numberOfLines = 1;
+    _descLable.textColor = [UIColor colorWithHex:0xFFFFFF];
+    [self addSubview:_descLable];
+    _descLable.text = @"该用户还没有填写描述...";
+    
+    _creditLabel = [UILabel new];
+    _creditLabel.font = [UIFont systemFontOfSize:13];
+    _creditLabel.textAlignment = NSTextAlignmentCenter;
+    _creditLabel.numberOfLines = 1;
+    _creditLabel.textColor = [UIColor colorWithHex:0xFFFFFF];
+    _creditLabel.text = @"积分：0";
+    [self addSubview:_creditLabel];
+    
+    [_userPortrait mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self).with.offset(screen_half_width-(userPortrait_width * 0.5));
+        make.top.equalTo(self).with.offset(view_userPortrait);
+        make.width.and.height.equalTo(@userPortrait_width);
+    }];
+    
+    [_genderImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.and.bottom.equalTo(self.userPortrait).with.offset(0);
+        make.width.and.height.equalTo(@genderImageView_width);
+    }];
+    
+    [_nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self).with.offset(32);
+        make.right.equalTo(self).with.offset(-32);
+        make.top.equalTo(self.userPortrait.mas_bottom).with.offset(8);
+    }];
+    
+    [_descLable mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self).with.offset(32);
+        make.right.equalTo(self).with.offset(-32);
+        make.top.equalTo(self.nameLabel.mas_bottom).with.offset(7);
+    }];
+    
+    [_creditLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self).with.offset(32);
+        make.right.equalTo(self).with.offset(-32);
+        make.top.equalTo(self.descLable.mas_bottom).with.offset(15);
+    }];
+    
+}
+
+@end
---
+++ iosapp/Models/OSC/OSCNewUser.m
@@ -0,0 +1,15 @@
+//
+//  OSCNewUser.m
+//  iosapp
+//
+//  Created by 李萍 on 16/8/17.
+//  Copyright © 2016年 oschina. All rights reserved.
+//
+
+#import "OSCNewUser.h"
+
+@implementation OSCNewUser
+
+
+
+@end
